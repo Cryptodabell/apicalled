cac4e83591dd57a857bf3ce060e59076be7b3aad
Kevin Mechalister <76608592+Cryptodabell@users.noreply.github.com>
2024-04-22 11:07:42 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-415d5dfb-7e11-4db4-b9e3-71106e28ac88/)
 
 
 echo "$curl_output" >> Update.tsx
